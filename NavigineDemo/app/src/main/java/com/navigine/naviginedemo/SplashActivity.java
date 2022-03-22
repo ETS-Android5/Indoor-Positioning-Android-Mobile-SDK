79b83953b8363be0537fccc7ad753630f5f3890f
PavelTychinin <p.tychinin@navigine.com>
2022-03-22 16:24:46 +0300

    migrate to android12

--- NavigineDemo/app/src/main/java/com/navigine/naviginedemo/SplashActivity.java
+++ NavigineDemo/app/src/main/java/com/navigine/naviginedemo/SplashActivity.java
@@ -49,7 +49,10 @@ public class SplashActivity extends Activity implements ActivityCompat.OnRequest
     ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION,
                                                            Manifest.permission.ACCESS_COARSE_LOCATION,
                                                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                                                           Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+                                                           Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                           Manifest.permission.BLUETOOTH_SCAN,
+                                                           Manifest.permission.BLUETOOTH_CONNECT,
+    }, 101);
   }
 
   @Override public void onBackPressed()
@@ -65,10 +68,12 @@ public class SplashActivity extends Activity implements ActivityCompat.OnRequest
                                  ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     boolean permissionStorage  = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)  == PackageManager.PERMISSION_GRANTED &&
                                  ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    boolean permissionBluetooth = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)  == PackageManager.PERMISSION_GRANTED &&
+                                  ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED;
     switch (requestCode)
     {
       case 101:
-        if (!permissionLocation || (D.WRITE_LOGS && !permissionStorage))
+        if (!permissionLocation || !permissionBluetooth || (D.WRITE_LOGS && !permissionStorage))
           finish();
         else
         {
@@ -82,6 +87,9 @@ public class SplashActivity extends Activity implements ActivityCompat.OnRequest
                   Intent intent = new Intent(mContext, MainActivity.class);
                   intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                   mContext.startActivity(intent);
+
+
+
                 }
                 @Override public void onFailed(int error)
                 {
